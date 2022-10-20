fddac7195820d58254eab4ec6b32c3d17555e68c
wfer0011 <116248576+wfer0011@users.noreply.github.com>
2022-10-20 17:31:43 +1100

    Add files via upload

---
+++ maps.js
@@ -0,0 +1,10 @@
+var vg_1 = "mismanaged_waste_cloropeth.vg.json";
+vegaEmbed("#abc", vg_1).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "Globalplasticsproduction_bar.vg.json";
+vegaEmbed("#barchart", vg_2).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
